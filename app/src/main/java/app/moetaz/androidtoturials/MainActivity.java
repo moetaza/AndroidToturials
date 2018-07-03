c2e666b225ce464ca64cb07e126848e79d71f6ef
moetaza <moetaz_ashraf82@outlook.com>
2018-07-03 14:25:40 +0200

    test

--- app/src/main/java/app/moetaz/androidtoturials/MainActivity.java
+++ app/src/main/java/app/moetaz/androidtoturials/MainActivity.java
@@ -3,6 +3,8 @@ package app.moetaz.androidtoturials;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.moetaz.androidtoturials.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
